dbf6be994a7c0036044ff9fdfc760cba3b44d0bb
Sacha Weatherstone <sachaw100@hotmail.com>
2024-06-22 13:56:05 +1000

    overhaul

---
+++ mod.ts
@@ -0,0 +1,11 @@
+// Channel
+export * as Channel from "./generated/ts/channel/channel_pb.ts";
+export * as HeartbeatChannel from "./generated/ts/channel/heartbeat_pb.ts";
+export * as PeeringChannel from "./generated/ts/channel/peering_pb.ts";
+export * as PluginChannel from "./generated/ts/channel/plugin_pb.ts";
+export * as SubscriptionChannel from "./generated/ts/channel/subscription_pb.ts";
+export * as SystemChannel from "./generated/ts/channel/system_pb.ts";
+
+// Plugin
+export * as MosaicPlugin from "./generated/ts/plugin/mosaic_pb.ts";
+export * as Plugin from "./generated/ts/plugin/plugin_pb.ts";
