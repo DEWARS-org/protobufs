24d9538c3d4e30ff1d134ed35d90fa20dce06354
Sacha Weatherstone <sachaw100@hotmail.com>
2024-08-03 11:42:39 +1000

    geodata improvements, move to bufbuild/protobuf 2.0

--- mod.ts
+++ mod.ts
@@ -1,9 +1,3 @@
-// service
-export * as GeoDataService from "./generated/ts/service/geodata_pb.ts";
-export * as HeartbeatService from "./generated/ts/service/heartbeat_pb.ts";
-export * as PluginService from "./generated/ts/service/plugin_pb.ts";
-export * as SystemService from "./generated/ts/service/system_pb.ts";
-
 // Geocoder
 export * as Geocoder from "./generated/ts/geocoder/geocoder_pb.ts";
 
@@ -13,3 +7,9 @@ export * as Peering from "./generated/ts/peering/peering_pb.ts";
 // Plugin
 export * as MosaicPlugin from "./generated/ts/plugin/mosaic_pb.ts";
 export * as Plugin from "./generated/ts/plugin/plugin_pb.ts";
+
+// service
+export * as GeoDataService from "./generated/ts/service/geodata_pb.ts";
+export * as HeartbeatService from "./generated/ts/service/heartbeat_pb.ts";
+export * as PluginService from "./generated/ts/service/plugin_pb.ts";
+export * as SystemService from "./generated/ts/service/system_pb.ts";
