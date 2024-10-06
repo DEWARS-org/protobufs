f71d12dd6a79d429c704a0f7c800b4b16e2e4b74
Sacha Weatherstone <sachaw100@hotmail.com>
2024-10-06 17:10:24 +1000

    Overhaul

---
+++ README.md
@@ -0,0 +1,7 @@
+#  DEWARS Protocol Buffers
+
+## Running Codegen locally
+
+```shell
+deno run -A npm:@bufbuild/buf@1.44.0/buf generate dewars
+```
--- packages/ts/mod.ts
+++ packages/ts/mod.ts
@@ -1,6 +1,12 @@
 // Geocoder
 export * as Geocoder from "./lib/geocoder/geocoder_pb.ts";
 
+// ML
+import * as Translation from "./lib/ml/translation_pb.ts";
+export const ML = {
+  Translation,
+};
+
 // Peering
 export * as Peering from "./lib/peering/peering_pb.ts";
 
@@ -9,7 +15,13 @@ export * as MosaicPlugin from "./lib/plugin/mosaic_pb.ts";
 export * as Plugin from "./lib/plugin/plugin_pb.ts";
 
 // service
-export * as GeoDataService from "./lib/service/geodata_pb.ts";
-export * as HeartbeatService from "./lib/service/heartbeat_pb.ts";
-export * as PluginService from "./lib/service/plugin_pb.ts";
-export * as SystemService from "./lib/service/system_pb.ts";
+import * as GeoData from "./lib/service/geodata_pb.ts";
+import * as Heartbeat from "./lib/service/heartbeat_pb.ts";
+import * as PluginService from "./lib/service/plugin_pb.ts";
+import * as System from "./lib/service/system_pb.ts";
+export const Service = {
+  GeoData,
+  Heartbeat,
+  Plugin: PluginService,
+  System,
+};
